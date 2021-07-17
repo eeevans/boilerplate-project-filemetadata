5b88622ff2b46d843b1cd60b8e49de0d26d1f358
eeevans <ed.eevans@gmail>
2021-07-16 21:50:34 -0500

    Initial commit

--- server.js
+++ server.js
@@ -1,17 +1,46 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
 require('dotenv').config()
+const multer = require('multer');
+const mime = require('mime');
 
-var app = express();
+const app = express();
 
 app.use(cors());
+//app.use(express.urlencoded({ extended: true }));
+//app.use(express.json);
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+  console.log("sending index.html")
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now())
+  }
+})
 
+
+var fs = require("fs"); // Load the filesystem module
+
+var upload = multer({ storage: storage })
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res, next) => {
+  const file = req.file
+  if (!file) {
+    const error = new Error('Please upload a file')
+    error.httpStatusCode = 400
+    return next(error)
+  }
+  var stats = fs.statSync(__dirname + "/public/uploads/" + file.filename)
+  var fileSizeInBytes = stats.size;
+  res.json({ name: file.originalname, type: mime.lookup(file.originalname), size: fileSizeInBytes });
+  next();
+})
 
 
 const port = process.env.PORT || 3000;
